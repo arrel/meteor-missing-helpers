ec1a39ff1cd350dc500e5d0b39f83c68d6560cd0
Michael Samoylov <michael.samoylov@gmail.com>
2015-01-14 17:33:07 +0200

    Fixed client/server scope, added proper dependencies

--- package.js
+++ package.js
@@ -1,13 +1,15 @@
 Package.describe({
   name: 'msamoylov:missing-helpers',
   summary: 'Missing Meteor UI helpers',
-  version: '1.0.1',
+  version: '1.0.2',
   git: 'https://github.com/msamoylov/meteor-missing-helpers.git'
 });
 
 Package.onUse(function(api) {
   // Dependencies
   api.versionsFrom('1.0');
+
+  api.use(['underscore', 'deps', 'templating', 'ui', 'blaze', 'ejson', 'reactive-var'], 'client');
 
   api.addFiles('utility.js');
   api.addFiles(['helpers.js'], 'client');
